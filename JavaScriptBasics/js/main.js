a65a54d14f4dbe1e92e8101fba00975d9e056270
DaikiKujirai <kuuuuuuujira@gmail.com>
2021-02-24 15:56:54 +0900

    object 20

---
+++ JavaScriptBasics/js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+{
+  0, 1, 2
+  Math.floor(Math.random() * 3)
+  0, ..., n
+  Math.floor(Math.random() * (n + 1))
+  min, ..., max
+  Math.floor(Math.random() * (max + 1 - min)) + min
+}
