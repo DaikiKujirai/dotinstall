651a5a621eaae10d11d63490dc67075074751101
DaikiKujirai <kuuuuuuujira@gmail.com>
2021-02-24 20:14:11 +0900

    [finish] object

--- JavaScriptBasics/js/main.js
+++ JavaScriptBasics/js/main.js
@@ -1,9 +1,44 @@
 'use strict';
 {
-  0, 1, 2
-  Math.floor(Math.random() * 3)
-  0, ..., n
-  Math.floor(Math.random() * (n + 1))
-  min, ..., max
-  Math.floor(Math.random() * (max + 1 - min)) + min
+ class Post {
+   constructor(text) {
+     this.text = text;
+     this.likeCount = 0;
+   }
+
+   show() {
+     console.log(`${this.text} ${this.likeCount} likes`);
+   }
+
+   like() {
+     this.likeCount++;
+     this.show();
+   }
+ }
+
+
+ class SponsoredPost extends Post{
+   constructor(text, sponsor) {
+     super(text);
+     this.sponsor = sponsor;
+   }
+
+   show() {
+     super.show();
+     console.log(`... sponsored by ${this.sponsor}`);
+   }
+
+   like() {
+     this.likeCount++;
+     this.show();
+   }
+ }
+
+  const posts = [
+    new Post('JavaScriptの勉強中...'),
+    new Post('プログラミング楽しい！'),
+    new SponsoredPost('3分動画でマスターしよう', 'dotinstall'),
+  ];
+  posts[2].show();
+  posts[2].like();
 }
