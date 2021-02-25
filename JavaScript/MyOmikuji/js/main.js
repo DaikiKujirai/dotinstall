ad6d7adf63980c69bb999a0bda291252ae56ce88
DaikiKujirai <kuuuuuuujira@gmail.com>
2021-02-25 22:33:48 +0900

    [finish] おみくじ

---
+++ JavaScript/MyOmikuji/js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+{
+  const btn = document.getElementById('btn');
+
+  btn.addEventListener('click', () => {
+    const results = ['大吉☆', '中吉', '末吉', '凶', '大凶']
+    btn.textContent = results[Math.floor(Math.random() * results.length)];
+  });
+
+
+}
